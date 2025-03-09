47bcbfe1da44c63bef39aa25692c6d09fa668eae
iglbv <ilyagolubev2021@gmail.com>
2025-03-09 19:04:31 +0500

    add storage provider 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
-// src/index.tsx or main.tsx
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
-import { StorageProvider } from "./providers/StorageProvider"; // Import the provider
-
+import { StorageProvider } from "./providers/StorageProvider";
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
